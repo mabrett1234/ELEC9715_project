47d991e7d4868490d370867b153b221c52586bda
Summer <ddanielhartt@gmail.com>
2026-04-19 11:52:38 +1000

    Renamed and made skeleton for amber (#14)

---
+++ VPP_amber.py
@@ -0,0 +1,13 @@
+#VPP_model_customer
+#To use: import VPP_model_customer as customer
+"""
+TODO: Code overview
+"""
+
+# Imports
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def bess_operation():
+    pass
